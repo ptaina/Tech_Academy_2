0e0ff108ac58e69f65ac978fefc6307eafa8dc2b
ptaina <tainapereirae27@gmail.com>
2024-06-21 14:03:24 -0300

    first commit

---
+++ paginas/erro.php
@@ -0,0 +1,30 @@
+<div class="container">
+    <img src="imagens/404.png" alt= "Página não encontrada">
+
+    <h2>A página que você está tentando acessar não exite!</h2>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
