db00061ea3e9beb20fc923d75c8ebfa0196ad21c
ptaina <tainapereirae27@gmail.com>
2024-06-26 01:35:29 -0300

    incluindo css na pagina de erro

--- paginas/erro.php
+++ paginas/erro.php
@@ -2,7 +2,6 @@
     <div class="container">
         <img src="imagens/error_novo.jpg" alt="Páginma de erro">
 
-        <h2>A página que você está tentando acessar não exite!</h2>
-
+        <h2>A página que você está tentando acessar não existe!</h2>
     </div>
 </div>
